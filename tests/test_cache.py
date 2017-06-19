86b2209dbb46e4fd73e29144eb11ca967e201c1d
Andrei Horak <linkyndy@gmail.com>
2017-06-19 16:26:30 +0200

    Write some tests for Cache

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,4 +1,34 @@
 from . import BaseTestCase
 
+from freon.cache import Cache
 
-class CacheTests(BaseTestCase):
+from mock import Mock, patch
+# class MockBackend(object)
+
+class StringSerializer(object):
+    def dumps(self, value):
+        return str(value)
+
+    def loads(self, value):
+        return str(value)
+
+class CacheTestCase(BaseTestCase):
+    def setUp(self):
+        self.cache = Cache()
+        # Set a default string serializer for easier testing
+        self.cache.serializer.dumps = lambda x: str(x)
+        self.cache.serializer.loads = lambda x: str(x)
+
+
+class GetTests(CacheTestCase):
+    def test_with_existing_expired_key(self):
+        self.cache.backend.get = lambda x: ('bar', True)
+        assert self.cache.get('foo') == 'bar'
+
+    def test_with_existing_not_expired_key(self):
+        self.cache.backend.get = lambda x: ('bar', False)
+        assert self.cache.get('foo') == 'bar'
+
+    def test_with_not_existing_key(self):
+        self.cache.backend.get = lambda x: (None, True)
+        assert self.cache.get('foo') is None
