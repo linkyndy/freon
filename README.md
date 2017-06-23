0a71cddf1e65cd75f8be78c5843d5947a0f01954
Andrei Horak <linkyndy@gmail.com>
2017-06-23 17:30:46 +0200

    Add more documentation

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # freon
-Keeps your data fresh just as freon keeps your food fresh in the fridge.
+Keeps your cached data fresh just as freon keeps your food fresh in the fridge.
+
+## Features
+
+* simple, powerful and extensible;
+* Redis and memory backends out of the box;
+* msgpack, JSON and pickle serializers out of the box;
+* dynamic values and TTLs;
+* avoids the dog-pile effect.
+
+## Basic usage
+
+```python
+from freon.cache import Cache
+
+cache = Cache()
+cache.set('foo', 'bar', 123)
+# Returns 'bar'
+
+cache.get('foo')
+# Returns 'bar'
+
+# approx. 123 seconds later...
+cache.get('foo')
+# Returns None
+```
--- freon/cache.py
+++ freon/cache.py
@@ -5,14 +5,29 @@ class Cache(object):
     """
     Connection to a cache backend
 
-    :param backend: Name of backend to connect to. Can be one of ``redis`` or
-     ``memory``
-    :param serializer: Name of serializer used to serialize/deserialize cached
-     values. Can be one of ``msgpack``, ``pickle`` or ``json``
+    :param backend: (optional) Name of backend to connect to. Can be one of ``redis`` or ``memory``. Defaults to ``redis``.
+    :type backend: string
+    :param serializer: (optional) Name of serializer used to serialize/deserialize cached values. Can be one of ``msgpack``, ``pickle`` or ``json``. Defaults to ``msgpack``.
+    :type serializer: string
+    :param default_ttl: (optional) Default TTL applied to items that don't have one associated. Value is expressed in seconds. Defaults to ``3600``.
+    :type default_ttl: integer
+    :param custom_encoder: (optional) Custom encoder to extend the serializer's default behaviour.
+    :type custom_encoder: None or callable
+    :param custom_decoder: (optional) Custom decoder to extend the serializer's default behaviour.
+    :type custom_decoder: None or callable
+    :param **kwargs: (optional) Extra arguments, passed to the selected backend.
+
+    Usage::
+
+      >>> from freon.cache import Cache
+      >>> cache = Cache()
+      >>> cache.get('foobar')
+      None
     """
 
     def __init__(self, backend='redis', serializer='msgpack', default_ttl=3600,
                  custom_encoder=None, custom_decoder=None, **kwargs):
+
         self.backend = self._load_backend(backend, **kwargs)
         self.serializer = self._load_serializer(serializer, custom_encoder, custom_decoder)
         self.default_ttl = default_ttl
@@ -20,6 +35,17 @@ class Cache(object):
     # TODO: configure if don't want to be constructivist and always return what
     # there is, even if expired
     def get(self, key):
+        """
+        Retrieves a cached entry by given key.
+
+        If the key doesn't exist, ``None`` is returned. If they key exists but
+        is expired, the cached entry is still returned.
+
+        :param key: Key of cached entry to be retrieved
+        :type key: string
+        :return: cached object or ``None``
+        """
+
         value, _ = self.backend.get(key)
 
         if value is None:
@@ -28,6 +54,33 @@ class Cache(object):
         return self.serializer.loads(value)
 
     def set(self, key, value, ttl=None):
+        """
+        Caches an entry by key and by TTL.
+
+        Value can be a callable, in which case it is executed before caching it.
+        This is particularly useful to defer creation of the entry to be cached
+        as much as possible (or maybe not even create it, if another thread
+        already does it).
+
+        TTL can be a callable, in which case it is executed before caching the
+        entry. It must accept an argument in this case, which will be passed the
+        value of the entry to be cached. This is particularly useful to defer
+        the computation of the TTL until it is really necessary, or to depend on
+        a deferred creation of the value (think of dynamically assigning a TTL
+        based on an attribute of the value).
+
+        This method avoids the dog-pile effect by using a lock. If some other
+        thread is already involved in caching the entry, it simply returns ``None``.
+
+        :param key: Key under which the entry will be cached
+        :type key: string
+        :param value: The actual entry to be cached
+        :type value: string or callable
+        :param ttl: (optional) TTL to be associated with the cached entry. Defaults to ``self.default_ttl``
+        :type ttl: integer, callable or ``None``
+        :return: cached object or ``None``
+        """
+
         lock = self.backend.get_lock(key)
 
         if not lock.acquire(False):
@@ -45,6 +98,19 @@ class Cache(object):
             lock.release()
 
     def get_or_set(self, key, new_value, ttl=None):
+        """
+        Tries to retrieve a cached entry by key. If the value does not exist,
+        it caches the entry by key and TTL.
+
+        :param key: Key of cached entry to be retrieved or under which the entry will be cached
+        :type key: string
+        :param new_value: The actual entry to be cached
+        :type new_value: string or callable
+        :param ttl: (optional) TTL to be associated with the cached entry. Defaults to ``self.default_ttl``
+        :type ttl: integer, callable or ``None``
+        :return: cached object, newly cached object or ``None``
+        """
+
         value, expired = self.backend.get(key)
 
         if value is None or expired:
@@ -54,15 +120,45 @@ class Cache(object):
         return self.serializer.loads(value)
 
     def delete(self, key):
+        """
+        Deletes a cached entry by key.
+
+        :param key: Key of cached entry to be deleted
+        :type key: string
+        :return: bool
+        """
+
         return self.backend.delete(key)
 
     def exists(self, key):
+        """
+        Checks whether a cached entry exists by key.
+
+        :param key: Key of cached entry to be checked
+        :type key: string
+        :return: bool
+        """
+
         return self.backend.exists(key)
 
     def get_expired(self):
+        """
+        Returns a list of expired keys.
+
+        :return: list of strings
+        """
+
         return self.backend.get_expired()
 
     def get_by_ttl(self, ttl):
+        """
+        Returns a list of keys that will expire within the specified threshold.
+
+        :param ttl: Number of seconds from the current time when retrieved keys will expire
+        :type ttl: integer
+        :return: list of strings
+        """
+
         return self.backend.get_by_ttl(ttl)
 
     def _load_backend(self, name, **config):
